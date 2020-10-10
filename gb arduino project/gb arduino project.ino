1a2308283092a3d208481d6e5b5a15cd6447002d
Jorge Garrido <jorge@jacoti.com>
2020-10-10 12:53:33 +0200

    Add Arduino project base

---
+++ gb arduino project/gb arduino project.ino
@@ -0,0 +1,16 @@
+/*
+  Golden Beetle
+
+  This program will receive signals through bluetooth LE and
+  activate motors with sent revs.
+
+  by Jorge Garrido <firezenk@gmail.com>
+*/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
