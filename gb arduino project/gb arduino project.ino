010eef02d64ecfb8f7aca453207a2bafd55c5b1e
Jorge Garrido <jorge@jacoti.com>
2020-11-04 16:57:10 +0100

    Update Arduino code to make external actions

--- gb arduino project/gb arduino project.ino
+++ gb arduino project/gb arduino project.ino
@@ -9,19 +9,39 @@
 
 byte ref[1];
 
+const int buzzer = 2; // buzzer at digital pin D2
+const int extLED = 5; // led at digital pin D5
+
 void setup() {
   Serial.begin(115200);  // initialize the Serial port
   pinMode(LED_BUILTIN, OUTPUT);  // initialize digital pin LED_BUILTIN as an output.
+
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
 }
 
 void loop() {
   if (Serial.available()) {
     Serial.readBytes(ref, 1);
-    if (ref[0] == 0x1) {
-      digitalWrite(LED_BUILTIN, HIGH);
-    } else {
-      digitalWrite(LED_BUILTIN, LOW);
+
+    switch(ref[0]) {
+      case 0x00:
+        digitalWrite(LED_BUILTIN, LOW); // INTERNAL LED LOW
+        break;
+      case 0x01:
+        digitalWrite(LED_BUILTIN, HIGH); // INTERNAL LED HIGH
+        break;
+      case 0x02:
+        tone(buzzer, 1000); // BEEP
+        delay(1000);
+        noTone(buzzer);
+        break;
+      case 0x03:
+        digitalWrite(extLED, HIGH); // EXTERNAL LED BLINK
+        delay(1000);
+        digitalWrite(extLED, LOW);
+        break;
     }
+
     Serial.print(ref[0], DEC);
   }
 }
